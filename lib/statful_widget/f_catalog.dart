9d66e3b14515621eccf78e7d66fe148539d3c201
saki8661 <saki8661@naver.com>
2024-01-02 11:11:23 +0900

    콜백함수 호출

--- lib/statful_widget/f_catalog.dart
+++ lib/statful_widget/f_catalog.dart
@@ -1,10 +1,35 @@
+import 'package:class_statement/common/models/vo_catalog.dart';
+import 'package:class_statement/common/w_catalog_item.dart';
 import 'package:flutter/material.dart';
 
 class CatalogWidget extends StatelessWidget {
-  const CatalogWidget({super.key});
+  // 통신을 통해 받은 데이터가 필요함
+  final List<Catalog> responseListData;
+
+  // 내 카트에 담은 데이터 정보 필요
+  final List<Catalog> catalogList;
+  final void Function(Catalog catalog) onPressedCatalog;
+
+  const CatalogWidget({super.key,
+    required this.responseListData,
+    required this.catalogList,
+    required this.onPressedCatalog});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+
+    return ListView.builder(
+      itemCount: responseListData.length,
+      itemBuilder: (context, index){
+        // 넘겨받은 리스트 중에 하나의 오브젝트이다.
+        Catalog catalog = responseListData[index];
+
+       return CatalogItem(
+           catalog: catalog,
+           isInCart: catalogList.contains(catalog),
+           onPressedCatalog: onPressedCatalog);
+      }
+    );
   }
 }
--- lib/statful_widget/s_home_1.dart
+++ lib/statful_widget/s_home_1.dart
@@ -17,10 +17,39 @@ class _HomeScreen1State extends State<HomeScreen1> {
   // 현재 성택된 index
   int currentIndex = 0;
   // 선택된 카탈로그 정보를 담을 수 있는 자료 구조 선언
-  List<Catalog> catalogList = [];
+  List<Catalog> catalogList = [
+    Catalog.name(color: Colors.red, title: "테스트 아이템1"),
+    Catalog.name(color: Colors.orange, title: "테스트 아이템2"),
+  ];
+
+  // 샘플데이터 (localDB, 통신)
+  late List<Catalog> responseListData;
+  @override
+  void initState() {
+    super.initState();
+    // initState <-- 위젯을 업데이트 하는 코드를 사용하면 안된다.(버그발생)
+    // 가능한 데이터 초기화 처리만 사용하자.
+    responseListData = catalogListSample;
+  }
+
+  // 콜백받을 메서드 선언
+  void onPressedCatalog(Catalog catalog){
+    print("여기 HomeScreen 위젯 이벤트 발생");
+    // 콜백이 일어나면 UI 업데이트 처리
+    setState(() {
+      if(catalogList.contains(catalog)) {
+        // 리스트에 object 포함 하고 있다면 삭제 처리
+        catalogList.remove(catalog);
+      }else{
+        catalogList.add(catalog); // add 처리
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    //
+    print("HomeScreen build() 함수 호출");
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -28,8 +57,15 @@ class _HomeScreen1State extends State<HomeScreen1> {
       ),
       body: IndexedStack(
         children: [
-          CatalogWidget(),
-          CartWidget(),
+        //   CatalogWidget(
+        //     responseListData: responseListData,
+        //     catalogList: catalogList,
+        //     onPressedCatalog: onPressedCatalog,
+        //   ),
+          CartWidget(
+            catList: catalogList,
+            onPressedCatalog: onPressedCatalog,
+          ),
         ],
       ),
     );
